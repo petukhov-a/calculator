bb59a6aabf8bf40ee4aa5b119d7cb312510a06f7
Anton <reopik@mail.ru>
2023-06-06 01:37:19 +0300

    made work number btns, delete btn and clear btn

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const calcBtnsWrapper = document.querySelector('.calc-buttons');
+const calcScreen = document.querySelector('.calc-screen');
 
 function addCalcBtn(btnName, gridRow, girdColumn, color='#e1e1e1') {
     const calcBtn = document.createElement('div');
@@ -18,9 +19,9 @@ addCalcBtn('X', 1, 4);
 addCalcBtn('+', 2, 4);
 addCalcBtn('-', 3, 4);
 addCalcBtn(',', 4, 4);
+addCalcBtn('=', 5, 4);
 
 addCalcBtn('0', 5, 2, '#fff');
-
 addCalcBtn('1', 2, 1, '#fff');
 addCalcBtn('2', 2, 2, '#fff');
 addCalcBtn('3', 2, 3, '#fff');
@@ -32,3 +33,19 @@ addCalcBtn('6', 3, 3, '#fff');
 addCalcBtn('7', 4, 1, '#fff');
 addCalcBtn('8', 4, 2, '#fff');
 addCalcBtn('9', 4, 3, '#fff');
+
+document.querySelectorAll('.calc-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (isFinite(btn.textContent)) {
+            calcScreen.textContent += btn.textContent;
+        }
+
+        if (btn.textContent === 'C') {
+            calcScreen.textContent = '';
+        }
+        
+        if (btn.textContent === 'DEL') {
+            calcScreen.textContent = calcScreen.textContent.slice(0, -1);
+        }
+    });
+});
