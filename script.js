03b576d050a479969135737fd70dfd32f9b705a2
Anton <reopik@mail.ru>
2023-06-06 23:44:28 +0300

    made plus btn work

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const calcBtnsWrapper = document.querySelector('.calc-buttons');
 const calcScreen = document.querySelector('.calc-screen');
+let result = 0;
+let isOperatorClicked = false;
 
 function addCalcBtn(btnName, gridRow, girdColumn, color='#e1e1e1') {
     const calcBtn = document.createElement('div');
@@ -25,11 +27,9 @@ addCalcBtn('0', 5, 2, '#fff');
 addCalcBtn('1', 2, 1, '#fff');
 addCalcBtn('2', 2, 2, '#fff');
 addCalcBtn('3', 2, 3, '#fff');
-
 addCalcBtn('4', 3, 1, '#fff');
 addCalcBtn('5', 3, 2, '#fff');
 addCalcBtn('6', 3, 3, '#fff');
-
 addCalcBtn('7', 4, 1, '#fff');
 addCalcBtn('8', 4, 2, '#fff');
 addCalcBtn('9', 4, 3, '#fff');
@@ -37,15 +37,43 @@ addCalcBtn('9', 4, 3, '#fff');
 document.querySelectorAll('.calc-btn').forEach(btn => {
     btn.addEventListener('click', () => {
         if (isFinite(btn.textContent)) {
-            calcScreen.textContent += btn.textContent;
+            if (calcScreen.textContent.length < 12) {
+                if (isOperatorClicked) {
+                    calcScreen.textContent = '';
+                    isOperatorClicked = false;
+                }
+                calcScreen.textContent += btn.textContent;
+            }
         }
 
         if (btn.textContent === 'C') {
             calcScreen.textContent = '';
+            result = 0;
         }
         
         if (btn.textContent === 'DEL') {
             calcScreen.textContent = calcScreen.textContent.slice(0, -1);
         }
+
+        if (btn.textContent === '+') {
+            result += +calcScreen.textContent;
+            calcScreen.textContent = result;
+            isOperatorClicked = true;
+        }
     });
 });
+
+// let operators = {
+//     '+': (result, number) => {
+//         console.log(result);
+//         result += number;
+//         calcScreen.textContent = result;
+//     }
+// }
+
+/*
+Что происходит при нажатии на "+"
+Запоминается записанное число. И сохраняется информации об операторе.
+Далее ввод второго числа. Второе число запоминается.
+При нажатии "=" используется информация об операторе и выполняется операция над числами.
+*/
